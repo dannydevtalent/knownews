487d1def0d0cf858c43f93eb18232b3fdb71098a
dannydevtalent <64434949+dannydevtalent@users.noreply.github.com>
2020-05-03 01:34:58 +0800

    Update 1.php

--- 1.php
+++ 1.php
@@ -365,7 +365,7 @@
 <head>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
-	<title>Football</title>
+	<title>Daily update</title>
 </head>
 <body style="width: 50%; margin: 0 auto">
 	
